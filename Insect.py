de4d2a198c3d64a383a46cf42be6d19a4a87c3cd
Anil Yadav <anil_yadav2@baylor.edu>
2021-09-13 15:47:40 -0500

    init

---
+++ Insect.py
@@ -0,0 +1,23 @@
+import random
+
+# The Coin class simulates a coin that can
+# be flipped.
+
+class inst:
+
+    def __init__(self):
+        self.legs = 4
+        self.wings = 2
+        self.flight=0
+
+
+    def flylength(self):
+        flight=random.randint(1, 10)
+        #return flylengths
+            
+
+    # The get_sideup method returns the value
+    # referenced by sideup.
+
+    def get_length(self):
+            return self.flight
---
+++ flycall.py
@@ -0,0 +1,32 @@
+import Insect as ins
+
+"""
+# The main function.
+def main():
+       # Create an object from the Coin class.
+        Inscect_name = ins.inst()   # this creates an instance called 'my_coin' of the class 'Coin()'
+        Inscect_name
+       # Display the side of the coin that is facing up.
+       print('lenght of flight:', Inscect_name.get_length())    # notice you do not have to supply the argument/parameter
+
+       # Toss the coin.
+
+
+
+       
+
+# Call the main function.
+
+main()
+"""
+
+mosquito=ins.inst()
+housefly=ins.inst()
+mosquito.flylength()
+housefly.flylength()
+
+print()
+
+
+
+
